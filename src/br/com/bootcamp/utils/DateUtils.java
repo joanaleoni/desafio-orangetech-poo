940f25d6505bc6a60778146a530420c9eacd211e
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2023-01-15 13:43:46 -0300

    Classe DateUtils - utilidades para manipulação de datas

---
+++ src/br/com/bootcamp/utils/DateUtils.java
@@ -0,0 +1,11 @@
+package br.com.bootcamp.utils;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author joana
+ */
+public class DateUtils {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+}
